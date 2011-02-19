b0d0f7408c2da245b64f5d6eb16fa2a8a39b2c11
David Koh <dkoh44@gmail.com>
2011-02-19 13:49:13 -0500

    Made it possible to change default behavior

--- src/edu/mit/compilers/le02/ast/ASTNodeVisitor.java
+++ src/edu/mit/compilers/le02/ast/ASTNodeVisitor.java
@@ -5,7 +5,7 @@ public abstract class ASTNodeVisitor {
 	/*
 	 * Helper method: visits the children of the node in order
 	 */
-	private void visitChildren(ASTNode node) {
+	protected void defaultBehavior(ASTNode node) {
 		for (ASTNode child : node.getChildren()) {
 			child.visit(this);
 		}
@@ -15,32 +15,35 @@ public abstract class ASTNodeVisitor {
 	 * Default behavior is to visit all nodes, but do nothing at each.
 	 * This way you only need to override the ones you care about to make
 	 * a new visitor.
+	 * 
+	 * Alternatively, you can override the default behavior method if you
+	 * need to
 	 */
-	public void accept(ArrayDeclNode node) {visitChildren(node);}
-	public void accept(ArrayLocationNode node) {visitChildren(node);}
-	public void accept(AssignNode node) {visitChildren(node);}
-	public void accept(BlockNode node) {visitChildren(node);}
-	public void accept(BooleanNode node) {visitChildren(node);}
-	public void accept(BoolOpNode node) {visitChildren(node);}
-	public void accept(BreakNode node) {visitChildren(node);}
-	public void accept(CallStatementNode node) {visitChildren(node);}
-	public void accept(CharNode node) {visitChildren(node);}
-	public void accept(ClassNode node) {visitChildren(node);}
-	public void accept(ContinueNode node) {visitChildren(node);}
-	public void accept(ForNode node) {visitChildren(node);}
-	public void accept(IfNode node) {visitChildren(node);}
-	public void accept(IntNode node) {visitChildren(node);}
-	public void accept(MathOpNode node) {visitChildren(node);}
-	public void accept(MethodCallNode node) {visitChildren(node);}
-	public void accept(MethodDeclNode node) {visitChildren(node);}
-	public void accept(MinusNode node) {visitChildren(node);}
-	public void accept(NotNode node) {visitChildren(node);}
-	public void accept(ReturnNode node) {visitChildren(node);}
-	public void accept(ScalarLocationNode node) {visitChildren(node);}
-	public void accept(StringNode node) {visitChildren(node);}
-	public void accept(SyscallArgNode node) {visitChildren(node);}
-	public void accept(SystemCallNode node) {visitChildren(node);}
-	public void accept(VarDeclNode node) {visitChildren(node);}
-	public void accept(VariableNode node) {visitChildren(node);}
+	public void accept(ArrayDeclNode node) {defaultBehavior(node);}
+	public void accept(ArrayLocationNode node) {defaultBehavior(node);}
+	public void accept(AssignNode node) {defaultBehavior(node);}
+	public void accept(BlockNode node) {defaultBehavior(node);}
+	public void accept(BooleanNode node) {defaultBehavior(node);}
+	public void accept(BoolOpNode node) {defaultBehavior(node);}
+	public void accept(BreakNode node) {defaultBehavior(node);}
+	public void accept(CallStatementNode node) {defaultBehavior(node);}
+	public void accept(CharNode node) {defaultBehavior(node);}
+	public void accept(ClassNode node) {defaultBehavior(node);}
+	public void accept(ContinueNode node) {defaultBehavior(node);}
+	public void accept(ForNode node) {defaultBehavior(node);}
+	public void accept(IfNode node) {defaultBehavior(node);}
+	public void accept(IntNode node) {defaultBehavior(node);}
+	public void accept(MathOpNode node) {defaultBehavior(node);}
+	public void accept(MethodCallNode node) {defaultBehavior(node);}
+	public void accept(MethodDeclNode node) {defaultBehavior(node);}
+	public void accept(MinusNode node) {defaultBehavior(node);}
+	public void accept(NotNode node) {defaultBehavior(node);}
+	public void accept(ReturnNode node) {defaultBehavior(node);}
+	public void accept(ScalarLocationNode node) {defaultBehavior(node);}
+	public void accept(StringNode node) {defaultBehavior(node);}
+	public void accept(SyscallArgNode node) {defaultBehavior(node);}
+	public void accept(SystemCallNode node) {defaultBehavior(node);}
+	public void accept(VarDeclNode node) {defaultBehavior(node);}
+	public void accept(VariableNode node) {defaultBehavior(node);}
 
 }
