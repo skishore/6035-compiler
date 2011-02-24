3aeace47b4d6ece7f76ce2baa0dbd070e32a1312
Maria Frendberg <mfrend@mit.edu>
2011-02-24 09:07:43 -0500

    Adding corrected SymbolTableGenerator

---
+++ src/edu/mit/compilers/le02/stgenerator/SymbolTableException.java
@@ -0,0 +1,21 @@
+package edu.mit.compilers.le02.stgenerator;
+
+import edu.mit.compilers.le02.ast.SourceLocation;
+
+public class SymbolTableException extends Exception {
+
+  private SourceLocation loc;
+
+  public SymbolTableException(SourceLocation location) {
+    loc = location;
+  }
+  public SymbolTableException(SourceLocation location, String msg) {
+    super(msg);
+    loc = location;
+  }
+
+  @Override
+  public String getMessage() {
+    return super.getMessage() + " at " + loc;
+  }
+}
