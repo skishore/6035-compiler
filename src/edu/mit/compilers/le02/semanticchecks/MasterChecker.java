1af98b5b75dd2ac6ff9b9471af2e12f35d0d3ab8
Shaunak Kishore <skishore@mit.edu>
2011-02-26 09:58:39 -0500

    MasterChecker which consolidates semantic checks

---
+++ src/edu/mit/compilers/le02/semanticchecks/MasterChecker.java
@@ -0,0 +1,25 @@
+package edu.mit.compilers.le02.semanticchecks;
+
+import edu.mit.compilers.le02.ast.ASTNode;
+
+public class MasterChecker {
+    /** Holds the MasterChecker singleton. */
+    private static MasterChecker instance;
+
+    /**
+     * Retrieves the MasterChecker singleton, creating if necessary.
+     */
+    public static MasterChecker getInstance() {
+        if (instance == null) {
+            instance = new MasterChecker();
+        }
+        return instance;
+    }
+
+    /**
+     * Runs all currently implemented semantic checks. 
+     */
+    public static void checkAll(ASTNode root) {
+        CheckDeclarations.checkDeclarations(root);
+    }
+}
